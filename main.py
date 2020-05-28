5e239b76e072da44fbcbdb737db7ce42bdac8ad2
pultarmi <milan.pultar@gmail.com>
2020-05-28 16:05:58 +0200

    fast push

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def get_available_gpus():
     local_device_protos = device_lib.list_local_devices()
     return [x.name for x in local_device_protos if x.device_type == 'GPU']
 print('---------------')
-print(len(get_available_gpus()))
+print(device_lib.list_local_devices())
 print('---------------')
 
 def main(config):
