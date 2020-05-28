363023fba299611001f96c461e532df4b1e97b2c
pultarmi <milan.pultar@gmail.com>
2020-05-28 16:21:43 +0200

    fast push

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ config, unparsed = get_config()
 print("-------------------------Deep Essential-------------------------")
 print("Note: To combine datasets, use .")
 
-def get_available_gpus():
-    local_device_protos = device_lib.list_local_devices()
-    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+# def get_available_gpus():
+#     local_device_protos = device_lib.list_local_devices()
+#     return [x.name for x in local_device_protos if x.device_type == 'GPU']
 # print('---------------')
 # print(device_lib.list_local_devices())
 # print('---------------')
