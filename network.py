b2ab94261a7b03923cb232d2c4510a99dcf4bd81
pultarmi <milan.pultar@gmail.com>
2020-05-30 23:16:39 +0200

    fast push

--- network.py
+++ network.py
@@ -337,7 +337,8 @@ class MyNetwork(object): #"""Network class """
 
     def restore(self):
         # print("Restoring from {}...".format(self.save_file_best))
-        name='./logs/Trained/main.py---gcn_opt=reweight_vanilla_sigmoid_softmax---bn_opt=gn---weight_opt=sigmoid_softmax---loss_multi_logit=1---use_fundamental=2---data_name=oan_outdoor---train_iter=50000---val_intv=10000000'
+        # name='./logs/Trained/main.py---gcn_opt=reweight_vanilla_sigmoid_softmax---bn_opt=gn---weight_opt=sigmoid_softmax---loss_multi_logit=1---use_fundamental=2---data_name=oan_outdoor---train_iter=50000---val_intv=10000000'
+        name='./logs/main.py---gcn_opt=reweight_vanilla_sigmoid_softmax---bn_opt=gn---weight_opt=sigmoid_softmax---loss_multi_logit=1---data_name=oan_outdoor---train_iter=400000---val_intv=10000000'
         # self.saver_best.restore(self.sess, 'logs/Trained/main.py---gcn_opt=reweight_vanilla_sigmoid_softmax---bn_opt=gn---weight_opt=sigmoid_softmax---loss_multi_logit=1---use_fundamental=2---data_name=oan_outdoor---train_iter=50000---val_intv=10000000/model-474000.data-00000-of-00001')
         # self.saver_best.restore(self.sess, './logs/Trained/main.py---gcn_opt=reweight_vanilla_sigmoid_softmax---bn_opt=gn---weight_opt=sigmoid_softmax---loss_multi_logit=1---use_fundamental=2---data_name=oan_outdoor---train_iter=50000---val_intv=10000000/model-474000.index')
         self.sess.run(tf.global_variables_initializer())
