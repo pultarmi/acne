d6e2d91451fc927280ec680b22629b2a878319cb
pultarmi <milan.pultar@gmail.com>
2020-05-30 22:12:46 +0200

    fast push

--- network.py
+++ network.py
@@ -335,8 +335,8 @@ class MyNetwork(object): #"""Network class """
         self.va_res_file_ours_ransac = os.path.join(self.res_dir_va, "valid", "va_res_ours_ransac.`txt")
 
     def restore(self):
-        print("Restoring from {}...".format(self.save_file_best))
-        self.saver_best.restore(self.sess, self.save_file_best)
+        # print("Restoring from {}...".format(self.save_file_best))
+        self.saver_best.restore(self.sess, 'logs/Trained/main.py---gcn_opt=reweight_vanilla_sigmoid_softmax---bn_opt=gn---weight_opt=sigmoid_softmax---loss_multi_logit=1---use_fundamental=2---data_name=oan_outdoor---train_iter=50000---val_intv=10000000/model-474000.data-00000-of-00001')
 
     def test_imw(self, x_in):
         print("Restoring from {}...".format(self.save_file_best))
