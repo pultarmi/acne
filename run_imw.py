44d4a18a8652fd8cf9788513429c6364b6d46e97
pultarmi <milan.pultar@gmail.com>
2020-06-02 18:58:01 +0200

    fast push

--- run_imw.py
+++ run_imw.py
@@ -2,12 +2,10 @@ import os
 import sys
 import numpy as np
 # import tensorflow as tf
-print('aaaaaaaaaaaa')
 import tensorflow.compat.v1 as tf
-print('bbbbbbbbbbbbb')
 tf.disable_v2_behavior()
-print('bbbbbbbbbbbbb')
 from network import MyNetwork
+from PIL import Image
 
 from parse import parse
 from tqdm import trange
@@ -19,32 +17,38 @@ from ops import tf_skew_symmetric
 from tests import test_process
 from config import get_config, print_usage
 config, unparsed = get_config()
+from glob import glob
 
 from read_write_model import read_model, qvec2rotmat
 from read_dense import read_array
 
-root = 'Datasets/Phototourism'
-seq = 'brandenburg_gate'
-src = root + '/' + seq
-print(f'Done')
+path = 'IMW/sacre_coeur'
 
-cameras, images, points = read_model(path=src + '/dense/sparse', ext='.bin')
-# print(cameras[294])
-# print(images[294].xys.shape)
-# print(images[295].xys)
-print(images[294])
-x_in = np.concatenate([images[294].xys, images[294].xys], axis=1)
-x_in = np.expand_dims(x_in, 0)
-x_in = np.expand_dims(x_in, 0)
-print(x_in)
-print(x_in.shape)
+# https://github.com/vcg-uvic/image-matching-benchmark/blob/master/example/training_data/parse_data.ipynb?fbclid=IwAR2prxKGOvm5mJPdjzH8XHEMR3oiE0IV9KgtshTK3lCyty-g3DjhFp9wGx8
 
-mynet = MyNetwork(config)
-mynet.restore()
-mynet.test_imw(x_in)
+paths = sort(glob(os.path.join(path, '*')))
+for p in paths:
+    img = Image.open(p)
 
-# self.x_in: xs_b,  # (?, 1, ?, 4)
-# self.y_in: ys_b,  # (?, ?, 2)
-# self.R_in: Rs_b,  # (?, 9)
-# self.t_in: ts_b,  # (?, 3)
+    # cameras, images, points = read_model(path=src + '/dense/sparse', ext='.bin')
+    # print(cameras[294])
+    # print(images[294].xys.shape)
+    # print(images[295].xys)
+    print(img.height)
+    x_in = np.concatenate([img.xys, img.xys], axis=1)
+    x_in = np.expand_dims(x_in, 0)
+    x_in = np.expand_dims(x_in, 0)
+    print(x_in)
+    print(x_in.shape)
+
+    mynet = MyNetwork(config)
+    mynet.restore()
+    mynet.test_imw(x_in)
+
+    # self.x_in: xs_b,  # (?, 1, ?, 4)
+    # self.y_in: ys_b,  # (?, ?, 2)
+    # self.R_in: Rs_b,  # (?, 9)
+    # self.t_in: ts_b,  # (?, 3)
+
+    break
 
