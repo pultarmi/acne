bc7cf911a1dca9056fa639ff29ffeff2dd045c6a
pultarmi <milan.pultar@gmail.com>
2020-06-04 22:53:23 +0200

    fast push

--- run_imw.py
+++ run_imw.py
@@ -50,8 +50,8 @@ mynet.restore()
 topnum = 1000
 
 paths = sorted(glob(os.path.join(path, '*')))
-for i,p1 in tqdm(enumerate(paths)):
-    for p2 in paths[i+1:]:
+for i,p2 in tqdm(enumerate(paths)):
+    for p1 in paths[i+1:]:
         kps1 = get_kps(p1)
         kps2 = get_kps(p2)
 
@@ -59,7 +59,7 @@ for i,p1 in tqdm(enumerate(paths)):
         # print(cameras[294])
         # print(images[294].xys.shape)
         # print(images[295].xys)
-        name =  os.path.splitext(os.path.basename(p2))[0] + '-' + os.path.splitext(os.path.basename(p1))[0]
+        name =  os.path.splitext(os.path.basename(p1))[0] + '-' + os.path.splitext(os.path.basename(p2))[0]
         match = matches.get(name).value
         kps1 = kps1[match[0]]
         kps2 = kps2[match[1]]
